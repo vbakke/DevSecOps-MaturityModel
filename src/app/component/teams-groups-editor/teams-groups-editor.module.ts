7124abbaaaf461bdfb0d0f116e4d7ccbb0a5dd4e
EMEAAD\vbakke <vegard.bakke@soprasteria.com>
2025-07-24 22:41:17 +0200

    Teams: Added some visual suguar to the new lists

--- src/app/component/teams-groups-editor/teams-groups-editor.module.ts
+++ src/app/component/teams-groups-editor/teams-groups-editor.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeamsGroupsEditorComponent } from './teams-groups-editor.component';
 import { SelectableListComponent } from './selectable-list.component';
+import { MaterialModule } from '../../material/material.module';
 
 @NgModule({
   declarations: [TeamsGroupsEditorComponent, SelectableListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
   exports: [TeamsGroupsEditorComponent]
 })
 export class TeamsGroupsEditorModule {}
